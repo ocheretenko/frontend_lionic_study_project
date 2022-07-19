25b91e471a39d11f802d8b31a2c1f513b5ebc63d
Alexey Ocheretenkp <b8er2la@outlook.com>
2022-07-20 00:31:18 +0300

    adaptive rework

---
+++ header-nav-menu.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let headerNav = document.querySelector('.header__nav');
+
+    document.querySelector('.header__show-popup-nav-btn').addEventListener('click', (event) => {
+        event.preventDefault();
+        headerNav.classList.add('header__nav-popup');
+    });
+
+    document.querySelector('.header__hide-popup-nav-btn').addEventListener('click',(event) => {
+        event.preventDefault();
+        headerNav.classList.remove('header__nav-popup');
+    });
+})
